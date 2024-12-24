6320a47f8af4bdd3cb0efc3174f5e557ec1abd34
abdullohaliaskarov2012 <aliaskarovabdulloh@gmail.com>
2024-12-24 05:25:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = prompt('Ismingizni kiriting')
+let b = prompt('Id ni kiriting')
+let c = prompt('Parolni kiriting')
+
+
+let um = {
+    name:a ,
+    id: b ,
+    passwor: c
+}
+console.log(um)
